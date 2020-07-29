adefdb9b26a40ada55616f7331d9f70c50139a7e
jolin0504 <zpl5231@psu.edu>
2020-07-29 09:25:16 -0400

    latest version

--- server.R
+++ server.R
@@ -5,6 +5,7 @@ library(shinyBS)
 library(plotly)
 library(corrplot)
 
+
 shinyServer(function(input, output,session) {
   
   # From https://github.com/beanumber/mdsr/blob/master/data/SAT_2010.rda
@@ -50,29 +51,57 @@ shinyServer(function(input, output,session) {
       #   geom_point()+xlim(c(45111,72734))+ylim(c(1473,1781))+ylab("Average Total SAT Score")+
       #   xlab("Average Teacher Salary")
       
-      p=ggplot(data=SAT_2010_plot2,aes(salary,total, col=SAT_grp, label=state
-      ))+geom_smooth(method="lm", se=F)+
-        labs(x="Teachers' salaries ($)", y="SAT scores")+geom_point(aes(text=paste("State:", state, "\n",
-                                                                              "New Salary:", salary,"\n",
-                                                                              'New SAT Score:', total,"\n",
-                                                                              '', SAT_grp)),size = 3, pch=21)
+      p<- ggplot(data=SAT_2010_plot2, aes(salary,total, col=SAT_grp, label=state))+
+        geom_point(aes(text=paste("State:", state, "\n", "New Salary:",
+                                  salary,"\n", 'New SAT Score:', total,"\n",
+                                  '', SAT_grp)),size = 3, pch=21)+
+        geom_smooth(method = "lm",se = F)+
+        labs(x="Teachers' salaries ($)", y="SAT scores")
+        
       
       
-      
-      pp=p + geom_smooth(method = "lm",se = F, color = "black", linetype = "longdash",lwd=1.5)+
+      pp <-p + 
+        geom_smooth(method = "lm",se = F, color = "black", linetype = "longdash",lwd=1.5)+
         scale_colour_manual(name='',values=c('Low SAT Participation State'='orange','High SAT Participation State'='blue',
-                                             'black'='black'))+theme_bw()+
+                                             'black'='black'))+
+        theme_bw()+
         theme(axis.title.x = element_text( face="bold"),
               axis.title.y = element_text(face="bold"),
               panel.background = element_blank(),
               legend.position="none",
               axis.line = element_line(color = 'black'))
       
-      ggplotly(pp,tooltip =  "text")%>% config(displayModeBar = F) 
-      
-      
+      ggplotly(pp,tooltip =  "text")%>% plotly::config(displayModeBar = F)
       
     })
+      
+      output$blue = renderText({
+        paste0("The blue line represents High SAT Participation States Regression.")
+      })
+      
+      output$orange = renderText({
+        paste0("The orange line represents Low SAT Participation States Regression.")
+      })
+      
+      output$black = renderText({
+        paste0("The black dotted line represents Overall Regression.")
+      })
+      
+      output$HLtable <- DT::renderDT(
+        HL <- data.frame(
+          State = c("California","Maryland","Massachusetts","New Jersey","Pennsylvania","Rhode Island",
+                    "Kansas", "Minnesota", "Nebraska","North Dakota","Tennessee","Wisconsin"),
+          Salary = c(71611, 67167, 72734, 68384, 62112, 62668, 
+                     48988, 55051, 48537, 45111, 48603, 53826),
+          Total = c(1517, 1502, 1547, 1506, 1473, 1477, 
+                    1752, 1781, 1746, 1733, 1712, 1778),
+          Sat_pct =c(53, 74, 89, 78, 73, 68, 
+                     7, 7, 5, 3, 10, 5)
+        )
+      )
+  
+    
+
 }
 )
 
--- ui.R
+++ ui.R
@@ -4,10 +4,10 @@ library(ggplot2)
 library(shinyBS)
 library(plotly)
 library(corrplot)
+library(boastUtils)
 
 
 dashboardPage(skin="purple",
-              
               #Title
               dashboardHeader(
                 title="Simpson's paradox",
@@ -18,7 +18,6 @@ dashboardPage(skin="purple",
                 ),
               #Sidebar
               dashboardSidebar(
-             
                 width = 250,
                 sidebarMenu(
                   id = "tabs",
@@ -38,10 +37,8 @@ dashboardPage(skin="purple",
                   tags$link(rel = "stylesheet", type = "text/css", 
                             href="https://educationshinyappteam.github.io/Style_Guide/theme/boast.css")
                 ),
-                
                 tabItems(
                   tabItem(tabName = "over",
-                          
                           h1("Simpsons Paradox"),
                           p("In this app you will explore Simpson's paradox.
                                       Simpson's paradox is a phenomenon in which a trend appears in different 
@@ -57,92 +54,88 @@ dashboardPage(skin="purple",
 
                                                "
                           ),
-                          
-                          p(tags$li("When you just look at the states with high SAT
+                          tags$ol(
+                            tags$li("When you just look at the states with high SAT
                                                        participation rates ( blue circles ), you will see a 
                                                        positive relationship. Similarly, if you just look 
                                                        at the states with low SAT participation 
-                                                       rates ( orange circles ), there is also a positive relationship.")),
-                          p(tags$li("Looking at all 12 states together, you will see
+                                                       rates ( orange circles ), there is also a positive relationship."),
+                            tags$li("Looking at all 12 states together, you will see
                                                        a negative relationship ( black line ). The difference 
                                                        between the black line and the orange and blue lines illustrates 
-                                                       Simpson's paradox.")),
-                          p(tags$li("What would the data look like if the participation rates were 
-                                                       more equal? Adjust the slider to see!")), 
-                         
+                                                       Simpson's paradox."),
+                            tags$li("What would the data look like if the participation rates were 
+                                                       more equal? Adjust the slider to see!")
+                          ),
                           br(),
                           h2("Instructions"),
-                          p(tags$li("Click the Explore button below to see the plot of original/actual paradox effect.")),
-                          p(tags$li("Move the slider to see how making the participation rates more equal lessen
-                                      the paradox effect.")),
+                          tags$ol(
+                            tags$li("Click the Explore button below to see the plot of original/actual paradox effect."),
+                            tags$li("Move the slider to see how making the participation rates more equal lessen
+                                      the paradox effect.")
+                          ),
                           div(style = "text-align: center",bsButton("explore", "Explore", icon("bolt"), size = "large")),
                           br(),
                           h2("Acknowledgements"),
                           p("This app was developed and programmed by Jinglin Feng and updated by Zhuolin Luo in 2020."),
-                          
-                         
-                            br(),
-                            br(),
-                            br(),
-                            div(class = "updated", "Last Update: 07/07/2020 by ZL.")
-                          
-                          
+                          br(),
+                          p(
+                          br(),
+                          br(),
+                          br(),  
+                          div(class = "updated", "Last Update: 07/24/2020 by ZL.")
+                          )
                   ),
                   
                   #Define the content contained within part 1 ie. tabname "first"
                   tabItem(tabName = "first",
-                          
                           fluidRow(
-                              
-                              column(6,
-                                 h3("Plot:"),
-                               
-                                 div(style = "position:relative;right:-6em;",
-                                 sliderInput("integer", label = div(style='width:216%;', 
-                                                                            div(style='float:left; width:50%', 'No Paradox Effect'), 
-                                                                           div(style='float:right;width:50%', 'Actual Paradox Effect')), 
-                                                                min = 0, max = 1, value = 1, width = '394px')),
-                                 bsPopover("integer", "", "Move the slider to see how the Simpson Paradox effect changes. Default as actual paradox effect, which is the true SAT participation rates in the dataset. No paradox effect is the case that when all states have equal SAT participation rates", place="right", options = list(container = "body")),
-                          
-                                 plotlyOutput("plot2"),
-                                 bsPopover("plot2", "", "Hover over a point to see more details", placement = "right"),
-                                 img(src="jinglin.jpg", width="70%"),
-                                 
-                                 h3("Challenge:"),
-                                 h4("How does the slope of the overall regression line change
-                                    as the SAT participation rate become more similar from State to State?")
-                                           
-                          
-                                
+                            column(6,
+                                   h3("Plot:"),
+                                   div(style = "position:relative;right:-6em;",
+                                       sliderInput("integer", label = div(style='width:216%;', 
+                                                                          div(style='float:left; width:50%', 'No Paradox Effect'),
+                                                                          div(style='float:right;width:50%', 'Actual Paradox Effect')),
+                                                   min = 0, max = 1, value = 1, width = '394px')),
+                                   bsPopover("integer", "", "Move the slider to see how the Simpson Paradox effect changes. Default as actual paradox effect, 
+                                           which is the true SAT participation rates in the dataset. No paradox effect is the case that when all states have 
+                                           equal SAT participation rates", place="right", options = list(container = "body")),
+                                   plotlyOutput("plot2"),
+                                   bsPopover("plot2", "", "Hover over a point to see more details", placement = "right"),
+                                   br(),br(),
+                                   wellPanel(
+                                    textOutput("blue"),
+                                    textOutput("orange"),
+                                    textOutput("black")
+                                 )
                             ),
                           
-                          column(4,offset = 1,
-                                 h3("Introduction:"),
-                                 box(width ="10.5%",background = "navy",
-                                     "This dataset is about the SAT results by state in 2010. There 
+                          column(6,
+                                 wellPanel(
+                                   h3("Introduction:"),
+                                   p("This dataset is about the SAT results by state in 2010. There 
                                      are 12 selected states with different average teachers' salaries, SAT 
                                      scores and SAT participation rates. The variable 'salary' is 
                                      the average teachers' salaries in US dollars ; The variable 'total' 
                                      is the state average SAT score ; The variable 'sat pct' is 
-                                     the percent of students taking the SAT in that state."),
-                                 
-                                 img(src="table.jpg", width = "306px", height = "400px", style="display: block; margin-left: auto; margin-right: auto;"),
-                                 br(),
-                                 h4(tags$div(
-                                   tags$strong("Low Group"),
-                                   ": States have SAT Participation Rate", 
-                                   tags$strong("less than 27%" ),
-                                   "(National Level)")),
-                                 
-                                 h4(tags$div(
-                                   tags$strong("High Group"),
-                                   ": States have SAT Participation Rate", 
-                                   tags$strong("greater than 27%" ),
-                                   "(National Level)"))
-                                 
+                                     the percent of students taking the SAT in that state.")
+                                 ),
+                                 DT::DTOutput("HLtable")
                                  
                                  )
-                          
+                  ),
+                  fluidRow(
+                    column(6,
+                           h3("Challenge:"),
+                           p("How does the slope of the overall regression line change
+                                    as the SAT participation rate become more similar from State to State?")),
+                    column(6,
+                           wellPanel(
+                                   p("Low Group:"),
+                                   p("States have SAT Participation Rate less than 27% (National Level)"),
+                                   p("High Group:"),
+                                   p("States have SAT Participation Rate greater than 27% (National Level)")
+                                 ))
                   )
               ),
               
